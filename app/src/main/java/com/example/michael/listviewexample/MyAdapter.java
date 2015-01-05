545e40b47916dfe111885357798b38e68a47cb80
Michael Brown <Michael@Michaels-Mac-mini.local>
2015-01-04 23:37:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/michael/listviewexample/MyAdapter.java
@@ -0,0 +1,38 @@
+package com.example.michael.listviewexample;
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.michael.listviewexample.R;
+
+class MyAdapter extends ArrayAdapter<String> {
+    public MyAdapter(Context context, String[] values) {
+        super(context, R.layout.row_layout2, values);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater theInflater = LayoutInflater.from(getContext());
+
+        View theView = theInflater.inflate(R.layout.row_layout2, parent, false);
+
+        String device = getItem(position);
+
+        TextView theTextView = (TextView) theView.findViewById(R.id.textView1);
+
+        theTextView.setText(device);
+
+        ImageView theImageView = (ImageView) theView.findViewById(R.id.imageView1);
+
+        theImageView.setImageResource(R.drawable.dot);
+
+        return theView;
+    }
+}
